d72ce5d1ef17281115172d342b65e6da7f46e0c0
austin1052 <austin1052@gmail.com>
2022-07-13 14:16:03 -0400

    fix db connection

--- db/client.js
+++ db/client.js
@@ -3,6 +3,9 @@ const { Client } = require("pg");
 const DB_NAME = "catnip-dev";
 const DB_URL =
   process.env.DATABASE_URL || `postgres://localhost:5432/${DB_NAME}`;
-const client = new Client(DB_URL);
+const client = new Client({
+  connectionString: DB_URL,
+  ssl: process.env.DATABASE_URL ? true : false
+});
 
 module.exports = client;
